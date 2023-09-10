b4404ad8c4f74c851c9d0a56981309d13c4edd6b
Uriel Kindermann Caminha <uriel.caminha@willbank.com.br>
2023-09-09 23:52:50 -0300

    feat: add prisma errors handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { providePrismaClientExceptionFilter } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { DiariesModule } from './diaries/diaries.module';
 import { GuestsDiariesModule } from './guests-diaries/guests-diaries.module';
+import { NotesModule } from './notes/notes.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { NotesModule } from './notes/notes.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { NotesModule } from './notes/notes.module';
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
     },
+    providePrismaClientExceptionFilter(),
   ],
 })
 export class AppModule {}
